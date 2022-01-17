78af9aa27afc59914c05eab1e6ed66e31d86b61f
dlwlrma123 <alexfinhart@gmail.com>
2022-01-18 07:53:45 +0800

    uclamp_assist: reduce cpu usage * based on our current cpuset configuration
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2130,11 +2130,11 @@ static void uclamp_set(struct kernfs_open_file *of,
 
 	static struct ucl_param tgts[] = {
 		{"audio-app",             "0", "50", 0, 0},
-		{"top-app",             "35", "100", 0, 1},
+		{"top-app",             "10", "100", 0, 1},
 		{"foreground",          "0", "100",  0, 0},
-		{"restricted",          "0",  "50",  0, 0},
-		{"background",          "0",  "50",  0, 0},
-		{"system-background",   "0",  "50",  0, 0},
+		{"restricted",          "0",  "40",  0, 0},
+		{"background",          "0",  "35",  0, 0},
+		{"system-background",   "0",  "40",  0, 0},
 		{"camera-daemon",       "0", "100", 0, 1},
 	};
 
